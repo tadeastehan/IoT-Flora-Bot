b9f60377712ffe3a9a6f62edf31fd0d4565ab7f0
tadeastehan <tadeastehan@gmail.com>
2024-12-18 19:59:20 +0100

    Add IoTFloraBot and SerialLogger libraries, implement connection and telemetry functions

---
+++ lib/IoTFloraBot/IoTFloraBot.h
@@ -0,0 +1,9 @@
+#ifndef IOTFLORABOT_H
+#define IOTFLORABOT_H
+
+// Function declarations
+void establishConnection();
+void connectToWiFi();
+void sendTelemetry();
+
+#endif // IOTFLORABOT_H
---
+++ src/iot_configs.h
@@ -0,0 +1,19 @@
+/*****************************************************************************************************************************
+**********************************    Author  : Ehab Magdy Abdullah                      *************************************
+**********************************    Linkedin: https://www.linkedin.com/in/ehabmagdyy/  *************************************
+**********************************    Youtube : https://www.youtube.com/@EhabMagdyy      *************************************
+******************************************************************************************************************************/
+
+// Enter Your Wifi and Password
+#define IOT_CONFIG_WIFI_SSID ""
+#define IOT_CONFIG_WIFI_PASSWORD ""
+
+// Azure IoT Hub
+#define IOT_CONFIG_IOTHUB_FQDN ""
+// Azure IoT Gub Device ID
+#define IOT_CONFIG_DEVICE_ID ""
+// Azure IoT Hub Device Key (primary key)
+#define IOT_CONFIG_DEVICE_KEY ""
+
+// Publish 1 message every 5 seconds
+#define TELEMETRY_FREQUENCY_MILLISECS 10000
